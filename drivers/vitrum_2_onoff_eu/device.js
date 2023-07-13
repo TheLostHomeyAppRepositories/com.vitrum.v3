f772a94cb01f2051b264556940e5d0ac7b68b1a1
bypo <oreste@dimaggio.it>
2023-07-13 23:51:03 +0200

    first working version
    first working version

--- drivers/vitrum_2_onoff_eu/device.js
+++ drivers/vitrum_2_onoff_eu/device.js
@@ -1,22 +1,51 @@
 'use strict';
 
-const ZwaveDevice = require('homey-meshdriver').ZwaveDevice;
+const { Device } = require('homey');
 
-class Vitrum2OnOffEUDevice extends ZwaveDevice {
-	async onMeshInit() {
-		//this.enableDebug();
+class MyDevice extends Device {
 
-		if(Object.keys(this.node.CommandClass).length < 5)
-		{
-			this.registerCapability('onoff', 'BASIC');
-			this.registerReportListener('BASIC', 'BASIC_SET', ( rawReport, parsedReport ) => {
-				if(rawReport.Value == 0)
-					this.setCapabilityValue('onoff', false);
-				else
-					this.setCapabilityValue('onoff', true);
-			});
-		}
-	}
+  /**
+   * onInit is called when the device is initialized.
+   */
+  async onInit() {
+    this.log('MyDevice has been initialized');
+  }
+
+  /**
+   * onAdded is called when the user adds the device, called just after pairing.
+   */
+  async onAdded() {
+    this.log('MyDevice has been added');
+  }
+
+  /**
+   * onSettings is called when the user updates the device's settings.
+   * @param {object} event the onSettings event data
+   * @param {object} event.oldSettings The old settings object
+   * @param {object} event.newSettings The new settings object
+   * @param {string[]} event.changedKeys An array of keys changed since the previous version
+   * @returns {Promise<string|void>} return a custom message that will be displayed
+   */
+  async onSettings({ oldSettings, newSettings, changedKeys }) {
+    this.log('MyDevice settings where changed');
+  }
+
+  /**
+   * onRenamed is called when the user updates the device's name.
+   * This method can be used this to synchronise the name to the device.
+   * @param {string} name The new name
+   */
+  async onRenamed(name) {
+    this.log('MyDevice was renamed');
+  }
+
+  /**
+   * onDeleted is called when the user deleted the device.
+   */
+  async onDeleted() {
+    this.log('MyDevice has been deleted');
+  }
+
 }
 
-module.exports = Vitrum2OnOffEUDevice;
+module.exports = MyDevice;
